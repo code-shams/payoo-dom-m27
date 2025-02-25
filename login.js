b56a58bf9031ebe369856d8a248dbfed8ef5ce5f
code-shams <code.meshams@gmail.com>
2025-02-25 19:08:41 +0600

    Create landing page
    i. updated the assets folder.
    ii. added static navbar, welcome section and
    add money section.

---
+++ login.js
@@ -0,0 +1,20 @@
+const loginBtn = document.getElementById('login-btn');
+const mobileNum = document.getElementById("mobile-number");
+const password = document.getElementById("password");
+loginBtn.addEventListener("click", function(event){
+    event.preventDefault();
+    // check if both of the input are of right length
+    if (mobileNum.value.length === 11 && password.value.length === 4){
+        // check if both input are not numbers
+        if (isNaN(+mobileNum.value) || isNaN(+password.value)){
+            alert("Invalid mobile number or password");
+        }
+        else{
+            window.location.href = "./main.html";
+        }
+    }
+    // hits when the inputs are not valid
+    else{
+        alert("Invalid mobile number or password");
+    }
+})
